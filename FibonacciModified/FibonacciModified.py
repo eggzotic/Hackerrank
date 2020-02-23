135e1b581d822372f67f1c89a88fab2527f7c022
Richard Shepherd <eggzotic@gmail.com>
2020-02-23 22:20:18 +0800

    FibonacciModified

---
+++ FibonacciModified/FibonacciModified.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fibonacciModified function below.
+def fibonacciModified(t1, t2, n):
+    fibMod = {1: t1, 2: t2}
+    for i in range(3, n+1):
+        fibMod[i] = fibMod[i - 2] + fibMod[i - 1] * fibMod[i - 1]
+    return fibMod[n]
+
+if __name__ == '__main__':
+    try:
+        fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    except KeyError:
+        fptr = sys.stdout
+
+    t1T2n = input().split()
+
+    t1 = int(t1T2n[0])
+
+    t2 = int(t1T2n[1])
+
+    n = int(t1T2n[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
